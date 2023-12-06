b75d6b0f46c63752f8914a71f3ce641aa57103c5
Max Dzhigrenyuk <dzigrenukmaksim@gmail.com>
2023-12-06 20:44:38 +0700

    Написали фукнцию syracuse_max

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ def syracuse_sequence(number):
             number_list.append(number)
     return number_list
 
+
+def syracuse_max(number_list):
+    number_max = 0
+    for elem_list in number_list:
+        if elem_list > number_max:
+            number_max = elem_list
+    return number_max
+
