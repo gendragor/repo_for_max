3b305c82ac56cb7555a57465511522cea8f7bf5b
Max Dzhigrenyuk <dzigrenukmaksim@gmail.com>
2023-12-06 20:37:10 +0700

    Написали фукнцию syracuse_sequence

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 """Гипотеза Коллатца"""
 
+
+def syracuse_sequence(number):
+    number = number
+    number_list = [number]
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+            number_list.append(number)
+        else:
+            number = number * 3 + 1
+            number_list.append(number)
+    return number_list
+
